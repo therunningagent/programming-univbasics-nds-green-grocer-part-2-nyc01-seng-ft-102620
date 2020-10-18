39c4989e725db671fd1bda02aee5aeec79e0247f
Luis Torres <luistjr@gmail.com>
2020-10-18 14:16:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,11 +1,7 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  index = 0 
   
-  while index < cart.length do 
-    current_item = cart[index_cart]
-    discounted
 end
 
 def apply_clearance(cart)
