583de0d4cf5db63986316889cf63aacec445aae2
Luis Torres <luistjr@gmail.com>
2020-10-18 14:22:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,7 @@ def apply_coupons(cart, coupons)
   counter = 0 
   
   while counter < coupons.length 
-    cart_item = find
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
   binding.pry
 end
 
